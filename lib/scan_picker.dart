17de647f05322883c6d8888e04b94318465030b7
heruodun <zjuxiaoxiaos@163.com>
2024-12-12 22:52:46 +0800

    one api

--- lib/scan_picker.dart
+++ lib/scan_picker.dart
@@ -1,9 +1,7 @@
 import 'dart:async';
-import 'dart:convert';
 import 'package:easyorder_mobile/http_client.dart';
 import 'package:easyorder_mobile/scan.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 import 'constants.dart';
 import 'user_data.dart';
